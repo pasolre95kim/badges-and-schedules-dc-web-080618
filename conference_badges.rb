6c155d11a517edd6d820682b8d6d46f0386fc906
Michelle Kim <pasolre95kim@gmail.com>
2018-07-14 00:37:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,6 @@ def badge_maker(name)
    "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
-  return(badge)
+def batch_badge_creator(attendees)
+  return "Hello, my name is #{attendees}"
 end
