0ed558d9ce537bd0842594b1fb94533be8bf9ad8
Michelle Kim <pasolre95kim@gmail.com>
2018-07-14 01:21:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 
 def assign_rooms(array)
   new_array = []
-  counter = 0
+  counter = 1
 
   array.each do |name|
     new_array.push ("Hello, #{name}! You'll be assigned to room #{counter}!")
