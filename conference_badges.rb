1be8280b6901654ef7c40b2f42d096018103db92
Michelle Kim <pasolre95kim@gmail.com>
2018-07-14 01:03:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(array)
 end
 
 
-def assign_rooms (name, room_assignments)
+def assign_rooms (array_1, array_2)
   new_array = []
   array.each do |name, room_assignments|
     new_array.push ("Hello, #{name}! You'll be assigned to room #{room_assignments}!")
